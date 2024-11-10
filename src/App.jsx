512fb8ed55899cad6ce2e107446322e77b31471e
Rezwan-react <mdrazwanislam8@gmail.com>
2024-11-10 19:36:02 +0600

    text

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import { Link } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="btn_shine">Get early access</h1>
+    </>
+  )
+}
+
+export default App
